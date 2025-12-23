01af8224fefd52371ece142478cb32283d46ba3d
Shiva84 <shiva91official@gmail.com>
2025-12-23 14:58:14 +0530

    configure frontend api for production

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://propel-backend-f8gz.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
